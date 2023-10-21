daaa0a371eeb67455040016f08f219befbb5a461
Hersh Patel <hgpatel715@gmail.com>
2023-10-20 23:48:54 -0400

    Adding negative case

--- kzg-stuff/test-kzg.py
+++ kzg-stuff/test-kzg.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     start_time = time.time()
     pi = kzg.proof(encoded_polynomial, point, setup_g1_points)
     t = time.time() - start_time
-    print("Generated KZG proof of evaluation (took %.2fs)" % t)
+    print("Generated KZG proof of P(x) evaluation (took %.2fs)" % t)
 
     # verifier can verify proof that some (x,y) point is on P(x)
     # with only commitment C, proof pi, the point in question
@@ -45,4 +45,31 @@ if __name__ == '__main__':
     assert kzg.verify(C, pi, point, setup_g2_point), "proof not verified"
     t = time.time() - start_time
     print("Verified KZG proof of evaluation (took %.2fs)" % t)
+    print("\n")
+    print("Now we will change our polynomial, to make sure the proof verification fails")
+
+    # Change the data we used to generate polynomial by appending a byte
+    changed_data = data + b'\x88'
+    assert (data != changed_data)
+    # Generate P'(x)
+    changed_points, changed_polynomial = kzg.encode_as_polynomial(changed_data)
+    assert (encoded_polynomial != changed_polynomial)
+
+    # choose some point on P'(x) to prove
+    reveal_index = random.randrange(len(changed_points))
+    reveal_point = changed_points[reveal_index]
+
+    # generate kzg proof that point is on polynomial
+    start_time = time.time()
+    changed_pi = kzg.proof(changed_polynomial, reveal_point, setup_g1_points)
+    t = time.time() - start_time
+    print("Generated KZG proof of P'(x) evaluation (took %.2fs)" % t)
+    print("Now verifying proof against previous commit (should fail), this takes the longest because the pairing operation is very expensive")
+    start_time = time.time()
+    # Previous commitment, with proof and point from slightly different polynomail
+    assert kzg.verify(C, changed_pi, reveal_point,
+                      setup_g2_point) == False, "proof somehow verified"
+    t = time.time() - start_time
+    print("Unable to verify KZG proof of evaluation (took %.2fs)" % t)
     print("Success! everything took %.2fs" % (time.time() - st))
+    print("\n")
