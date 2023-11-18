76a357c82c21bcfc16a715c9a36aa2c496b0ab79
Hersh Patel <hgpatel715@gmail.com>
2023-11-18 00:45:17 -0500

    Adding sequest of keys

--- stealth_address.py
+++ stealth_address.py
@@ -35,29 +35,47 @@ class MainKey:
         self.secret_key = random_scalar()
         self.public_key = secp256k1.multiply(secp256k1.G, self.secret_key)
 
-    def generate_stealth_address(self) -> tuple["PlainPoint2D", int, "PlainPoint2D"]:
+    def generate_stealth_address(self, num_addresses: int) -> list[tuple["PlainPoint2D", int, "PlainPoint2D"]]:
         ephemeral_key = random_scalar()
-        ephemeral_public_key = secp256k1.multiply(secp256k1.G, ephemeral_key)
 
-        shared_secret_1 = secp256k1.multiply(
-            ephemeral_public_key, self.secret_key)
-        shared_secret_2 = secp256k1.multiply(self.public_key, ephemeral_key)
-        assert shared_secret_1 == shared_secret_2
-        ssx, ssy = shared_secret_1
-        str_sxy = f"{ssx}{ssy}"
-        s = string_to_number(str_sxy)
-        sG = secp256k1.multiply(secp256k1.G, s)
+        keys = []
 
-        stealth_private_key = s + self.secret_key
-        stealth_public_key = secp256k1.add(sG, self.public_key)
+        for _ in range(num_addresses):
 
-        return (ephemeral_public_key, stealth_private_key, stealth_public_key)
+            ephemeral_public_key = secp256k1.multiply(
+                secp256k1.G, ephemeral_key)
+
+            shared_secret_1 = secp256k1.multiply(
+                ephemeral_public_key, self.secret_key)
+            shared_secret_2 = secp256k1.multiply(
+                self.public_key, ephemeral_key)
+            assert shared_secret_1 == shared_secret_2
+            ssx, ssy = shared_secret_1
+            str_sxy = f"{ssx}{ssy}"
+            s = string_to_number(str_sxy)
+            sG = secp256k1.multiply(secp256k1.G, s)
+
+            stealth_private_key = s + self.secret_key
+            stealth_public_key = secp256k1.add(sG, self.public_key)
+
+            keys.append(
+                (ephemeral_public_key, stealth_private_key, stealth_public_key))
+
+            ephemeral_key = (ephemeral_key*ephemeral_key) % secp256k1.N
+
+        return keys
 
 
 if __name__ == "__main__":
     key = MainKey()
-    ephemeral_public_key, stealth_private_key, stealth_public_key = key.generate_stealth_address()
-    print("Ephemeral Public Key: \n\n\tX:%i\n\tY:%i\n" % ephemeral_public_key)
-    print("Stealth Secret Key: \n\n\t%i\n" % stealth_private_key)
-    print("Stealth address: \n\n\t%s\n" %
-          secp256k1.pub_to_address(stealth_public_key))
+    keys = key.generate_stealth_address(10)
+    for i, key in enumerate(keys):
+        print("Key: %i" % i)
+        ephemeral_public_key, stealth_private_key, stealth_public_key = key
+        print("Ephemeral Public Key: \n\n\tX:%i\n\tY:%i\n" %
+              ephemeral_public_key)
+        print("Stealth Secret Key: \n\n\t%i\n" % stealth_private_key)
+        print("Stealth address: \n\n\t%s\n" %
+              secp256k1.pub_to_address(stealth_public_key))
+
+        print("-------------\n")
